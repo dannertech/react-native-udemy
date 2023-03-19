feb2ccb017ded9b600fff79a55124073392310a6
dannertech <danner@danner.tech>
2023-03-19 17:52:30 -0400

    imported ImageDetail into ImageScreen.js

---
+++ src/Components/ImageDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const ImageDetail = () => {
+    return (
+        <View>
+            <Text>Image Detail Component</Text>
+        </View>
+    )
+};
+
+export default ImageDetail;
