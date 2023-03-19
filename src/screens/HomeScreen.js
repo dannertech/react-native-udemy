e818410064a263245391ded8a2d9df34a12ea63e
dannertech <danner@danner.tech>
2023-03-18 21:56:27 -0400

    added ComponentsScreen to Home page

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
+import ComponentsScreen from "./ComponentsScreen";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hi There!</Text>;
+  return (
+  <ComponentsScreen />
+    )
 };
 
 const styles = StyleSheet.create({
