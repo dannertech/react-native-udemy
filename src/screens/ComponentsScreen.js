7883249e827f9e362c1deea5c35e204accbfebe1
dannertech <danner@danner.tech>
2023-03-18 22:25:38 -0400

    completed first simple challenge

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,17 +2,22 @@ import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 
 const ComponentsScreen = () => {
+    const userName = "Danner";
     return (
         <View>
-            <Text style={styles.textStyle}>This is the Components Screen</Text>
+            <Text style={styles.header}>Getting Started with React Native</Text>
+            <Text styles={styles.name}>{userName}</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    textStyle: {
-        fontSize: 30,
+    header: {
+        fontSize: 45
+    },
+    name: {
+        fontSize: 20
     }
-});
+})
 
 export default ComponentsScreen;
