e996ea24cad98dc78586e0dbc73f99e249ece194
dannertech <danner@danner.tech>
2023-03-24 19:04:11 -0400

    added CounterScreen to navigation

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
+
+const CounterScreen = () => {
+    return (
+        <View>
+           <Text>This is the Counter Screen</Text>
+        </View>
+    )
+};
+
+export default CounterScreen;
