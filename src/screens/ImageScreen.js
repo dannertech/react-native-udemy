eb223f859dcd0ce8564f1924c758e17b407a2a47
dannertech <danner@danner.tech>
2023-03-24 19:02:17 -0400

    added CounterScreen.js

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -3,12 +3,15 @@ import {StyleSheet, View, Text} from 'react-native';
 import ImageDetail from '../Components/ImageDetail';
 
 const ImageScreen = () => {
+    const beach = require('../../assets/images/beach.jpg');
+    const forest = require('../../assets/images/forest.jpg');
+    const mountain = require('../../assets/images/mountain.jpg');
+
     return (
         <View>
-            <ImageDetail />
-            <ImageDetail />
-            <ImageDetail />
-            <ImageDetail />
+            <ImageDetail title="Beach Image" image={beach}/>
+            <ImageDetail title="Forest Image" image={forest}/>
+            <ImageDetail title="Mountain Image" image={mountain}/>
         </View>
     )
 };
