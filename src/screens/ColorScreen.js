126f2726257b6d49996dd286923f8341b82f3099
dannertech <danner@danner.tech>
2023-03-28 02:15:38 -0400

    added a button to ColorScreen

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -4,6 +4,7 @@ import {StyleSheet, View, Text, Button} from 'react-native';
 const ColorScreen = () => {
     return (
         <View>
+            <Button title="Add a Color"/>
             <Text>This is the Color Screen</Text>
         </View>
     )
