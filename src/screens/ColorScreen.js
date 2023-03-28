cb14df7a3f16df71c37b1ff0f2833c46e81fe89f
dannertech <danner@danner.tech>
2023-03-28 02:45:14 -0400

    ColorScreen adds random colors to colors state array and displays them using a FlatList

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, Button, FlatList} from 'react-native';
 
 const ColorScreen = () => {
+    const [colors, setColors] = useState([]);
+
+    function generateColor(){
+        const red = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+
+        
+        setColors([...colors, `rgb(${red}, ${green}, ${blue})`])
+        console.log(colors);
+    }
     return (
         <View>
-            <Button title="Add a Color"/>
+            <Button title="Add a Color" onPress={() => generateColor()}/>
             <Text>This is the Color Screen</Text>
+            <FlatList
+            data={colors}
+            keyExtractor={(item) => item}
+            renderItem={({item}) => {
+                console.log(item)
+                return <View style={{height: 100, width: 100, backgroundColor: item}}>
+                        <Text>This is a square</Text>
+                    </View>
+            }}
+            />
         </View>
     )
 };
