e643f36277f990a69749e6388d2199f1e293de54
dannertech <danner@danner.tech>
2023-03-28 13:31:07 -0400

    refactored code so that buttons stay within 0 and 255 rgb values

--- src/screens/ColorBoxScreen.js
+++ src/screens/ColorBoxScreen.js
@@ -7,19 +7,19 @@ const ColorBoxScreen = () => {
     const [green, setGreen] = useState(0);
     const [blue, setBlue] = useState(0);
 
-    console.log(blue);
+    
     return(
         <View>
             <View style={{height: 100, width: 100, backgroundColor: `rgb(${red}, ${green}, ${blue})`, alignSelf: 'center'}}>
                 <Text>Your Random Color</Text>
             </View>
             
-            <ColorButtonComponent title="Increase Red" pressFunction={() => setRed(red + 20)}/>
-            <ColorButtonComponent title="Decrease Red" pressFunction={() => setRed(red - 20)}/>
-            <ColorButtonComponent title="Increase Green" pressFunction={() => setGreen(green + 20)}/>
-            <ColorButtonComponent title="Decrease Green" pressFunction={() => setGreen(green + 20)}/>
-            <ColorButtonComponent title="Increase Blue" pressFunction={() => setBlue(blue + 20)}/>
-            <ColorButtonComponent title="Decrease Blue" pressFunction={() => setBlue(blue - 20)}/>
+            <ColorButtonComponent title="Increase Red" pressFunction={() => setRed(red + 20 < 255 ? red + 10 : 255)}/>
+            <ColorButtonComponent title="Decrease Red" pressFunction={() => setRed(red - 20 > 0 ? red - 10 : 0)}/>
+            <ColorButtonComponent title="Increase Green" pressFunction={() => setGreen(green + 20 < 255 ? green + 20 : 255)}/>
+            <ColorButtonComponent title="Decrease Green" pressFunction={() => setGreen(green - 20 > 0 ? green - 20 : 0)}/>
+            <ColorButtonComponent title="Increase Blue" pressFunction={() => setBlue(blue + 20 < 255 ? blue + 20 : 255)}/>
+            <ColorButtonComponent title="Decrease Blue" pressFunction={() => setBlue(blue - 20 > 0 ? blue - 20 : 0)}/>
         </View>
     )
 };
