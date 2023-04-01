a6421b19e9ef8d0c86bc7260b22bb36760e3e8e3
dannertech <danner@danner.tech>
2023-04-01 15:59:09 -0400

    deactivated autoCorrect and autoCapitalize'

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {TextInput, Text, View, StyleSheet} from 'react-native';
 
 const TextScreen = () => {
+    const [name, setName] = useState('');
     return (
         <View>
             <Text>Enter Name:</Text>
-            <TextInput placeholder='Name'/>
+            <TextInput placeholder='Name' autoCapitalize='false' autoCorrect={false} onChange={(text) => setName(text)} style={styles.input}/>
         </View>
     )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 10
+    }
+});
 
 export default TextScreen;
