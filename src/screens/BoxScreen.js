3a695ca20c267d5e1275ca9f49713fa892d96cb7
dannertech <danner@danner.tech>
2023-04-01 20:39:01 -0400

    played around with flex-box

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,8 +3,10 @@ import {View, StyleSheet, Text} from 'react-native';
 
 const BoxScreen = () => {
     return(
-        <View>
-            <Text>Box Screen</Text>
+        <View style={{display: 'flex', borderWidth: 5, borderColor: 'black', height: '100%',justifyContent: 'center', alignItems: 'center'}}>
+            <View style={{height: 150, width: 150, backgroundColor: 'gold', borderWidth: 2, borderColor: 'red'}}></View>
+            <View style={{height: 150, width: 150, backgroundColor: 'purple'}}></View>
+            <View style={{height: 150, width: 150, backgroundColor: 'red'}}></View>
         </View>
     )
 };
